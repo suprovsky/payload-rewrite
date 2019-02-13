02fdf82f157e80f696cbe3da07cd06c90be3cf7b
Juan de Urtubey <jdeurt@gmail.com>
2019-02-12 21:40:52 -0600

    Removed overkill licenses.

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,21 +1,3 @@
-/*
-    Payload, a Discord bot made for the TF2 community.
-    Copyright (C) 2019  Juan de Urtubey
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as published
-    by the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
-
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
-
 export * from "./Command";
 export * from "./AutoResponse";
 export * from "./Bot";
