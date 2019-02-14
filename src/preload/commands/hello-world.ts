497a595e5eb2c3935f64bc4348b857048364ec10
Juan de Urtubey <jdeurt@gmail.com>
2019-02-13 18:18:28 -0600

    Updated hellow world for ease of usage with help command.

--- src/preload/commands/hello-world.ts
+++ src/preload/commands/hello-world.ts
@@ -1,14 +1,10 @@
 import { Bot } from "../../types";
 import { Message } from "discord.js";
-
-/**
- * @command
- * [NAME] hello world
- * [DESCRIPTION] A test command that also serves as a template for other commands.
- * [USAGE] %PREFIX%%NAME%
- */
+import config from "../../../secure-config";
 
 export const name = "hello world";
+export const description = "A test command that also serves as a template for other commands.";
+export const usage = config.PREFIX + name;
 
 export function run(bot: Bot, msg: Message) {
     msg.channel.send("Hello World!");
