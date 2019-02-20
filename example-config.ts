38d0d427fcb6c811a959dcd861f5608d64ddf927
Juan de Urtubey <jdeurt@gmail.com>
2019-02-20 08:51:58 -0600

    Added loading image path to config.

--- example-config.ts
+++ example-config.ts
@@ -2,7 +2,9 @@ const config = {
     TOKEN: "TOKEN_HERE",
     PREFIX: "pls ",
 
-    MONGODB_URI: ""
+    MONGODB_URI: "",
+
+    LOADING_IMG: __dirname + "/src/static/loading.png"
 };
 
 export default config;
