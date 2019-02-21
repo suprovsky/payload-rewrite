517eaa9eef1202feb8f880efde42919e0b9edf32
Juan de Urtubey <jdeurt@gmail.com>
2019-02-20 21:36:58 -0600

    Added servers to User model.

--- src/models/User.ts
+++ src/models/User.ts
@@ -3,13 +3,21 @@ import mongoose from "mongoose";
 export type UserModel = mongoose.Document & {
     id?: string,
 
-    steamID?: string
+    steamID?: string,
+
+    servers?: Array<{name: string, address: string, rconPassword: string}>
 };
 
 const userSchema = new mongoose.Schema({
     id: String,
 
-    steamID: String
+    steamID: String,
+
+    servers: [{
+        name: String,
+        address: String,
+        rconPassword: String
+    }]
 });
 
 export const User = mongoose.model("User", userSchema);
