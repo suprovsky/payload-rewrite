d7bbd619b202d1592d86a0d53cc171d2d99fd227
Juan de Urtubey <jdeurt@gmail.com>
2019-02-21 08:34:04 -0600

    Added Servers type.

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 
+export type Servers = Array<{name: string, address: string, rconPassword: string}>;
+
 export type UserModel = mongoose.Document & {
     id?: string,
 
     steamID?: string,
 
-    servers?: Array<{name: string, address: string, rconPassword: string}>
+    servers?: Servers
 };
 
 const userSchema = new mongoose.Schema({
