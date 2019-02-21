399657e510e9f35ae7cf6efedd0a00e224c1ad75
Juan de Urtubey <jdeurt@gmail.com>
2019-02-20 19:03:08 -0600

    Added permissions checking for later.

--- src/preload/commands/hello-world.ts
+++ src/preload/commands/hello-world.ts
@@ -5,6 +5,8 @@ import config from "../../../secure-config";
 export const name = "hello";
 export const description = "A test command that also serves as a template for other commands.";
 export const usage = config.PREFIX + name;
+export const permissions = ["SEND_MESSAGES"];
+export const canBeExecutedBy = ["SEND_MESSAGES"];
 
 export function run(bot: Bot, msg: Message) {
     msg.channel.send("Hello World!");
