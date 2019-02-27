f4be654e6b78349125b66f443dcbe7d83c4fae6b
Juan de Urtubey <jdeurt@gmail.com>
2019-02-26 21:14:08 -0600

    Made tag handling better.

--- src/preload/commands/r34.ts
+++ src/preload/commands/r34.ts
@@ -3,11 +3,11 @@ import { Message, TextChannel } from "discord.js";
 import config from "../../../secure-config";
 import xml2js from "xml2js";
 import got from "got";
-import { sliceCmd } from "../../utils/command-parsing";
+import { sliceCmd, getArgs } from "../../utils/command-parsing";
 
 export const name = "r34";
 export const description = "Finds an image on r34 based on search tags. Only works in NSFW channels with pl_x in their topic.";
-export const usage = config.PREFIX + name;
+export const usage = config.PREFIX + name + " [tag 1] [tag 2]...";
 export const permissions = ["SEND_MESSAGES", "EMBED_LINKS"];
 export const canBeExecutedBy = ["SEND_MESSAGES"];
 export const zones = ["text", "dm"];
@@ -17,8 +17,10 @@ export async function run(bot: Bot, msg: Message) {
 
     if (!(msg.channel as TextChannel).topic.includes("pl_x")) return msg.channel.send("This command cannot be executed in this channel. Use `" + config.PREFIX + "help " + name + "` to see how to allow this command here.");
 
+    let args = getArgs(sliceCmd(msg, name)).map(arg => arg.replace(/ +/g, "_"));
+
     return new Promise(async resolve => {
-        let resp = await got("https://rule34.xxx/index.php?page=dapi&s=post&q=index&tags=" + sliceCmd(msg, name).replace(/ +/g, "+"));
+        let resp = await got("https://rule34.xxx/index.php?page=dapi&s=post&q=index&tags=" + args.join("+"));
 
         let xml = resp.body;
         console.log(resp.requestUrl);
