16d52cfc4bedd8f115816ec9ca6e05c0e3ce5904
Juan de Urtubey <jdeurt@gmail.com>
2019-04-01 10:23:59 -0500

    Fixed payload not stopping its typing with pls link.

--- src/preload/commands/link.ts
+++ src/preload/commands/link.ts
@@ -12,36 +12,52 @@ export const canBeExecutedBy = ["SEND_MESSAGES"];
 export const zones = ["text", "dm"];
 
 export async function run(bot: Bot, msg: Message) {
-    let testString = msg.content.slice(config.PREFIX.length + name.length).trim();
+    return new Promise(async resolve => {
+        let testString = msg.content.slice(config.PREFIX.length + name.length).trim();
 
-    let steamIDTestResult = await ensureSteamID(testString);
+        let steamIDTestResult = await ensureSteamID(testString);
 
-    if (!steamIDTestResult) return msg.channel.send(`\`${testString}\` is not a valid Steam ID.`);
-
-    User.findOne({
-        id: msg.author.id
-    }, (err, user: UserModel) => {
-        if (err) return msg.channel.send("Error linking your Discord and Steam accounts. This is most likely a problem with the database.");
-
-        if (!user) {
-            user = new User({
-                id: msg.author.id,
-                steamID: steamIDTestResult
-            });
-
-            user.save(err => {
-                if (err) return msg.channel.send("Error linking your Discord and Steam accounts. This is most likely a problem with the database.");
-
-                msg.channel.send(`Successfully linked \`${steamIDTestResult}\` to \`${msg.author.tag}\`.`);
-            });
-        } else {
-            user.steamID = steamIDTestResult;
-
-            user.save(err => {
-                if (err) return msg.channel.send("Error linking your Discord and Steam accounts. This is most likely a problem with the database.");
-
-                msg.channel.send(`Successfully overrode old Steam ID with \`${steamIDTestResult}\` for \`${msg.author.tag}\`.`);
-            });
+        if (!steamIDTestResult) {
+            msg.channel.send(`\`${testString}\` is not a valid Steam ID.`);
+            return resolve();
         }
+
+        User.findOne({
+            id: msg.author.id
+        }, (err, user: UserModel) => {
+            if (err) {
+                msg.channel.send("Error linking your Discord and Steam accounts. This is most likely a problem with the database.");
+                return resolve();
+            }
+
+            if (!user) {
+                user = new User({
+                    id: msg.author.id,
+                    steamID: steamIDTestResult
+                });
+
+                user.save(err => {
+                    if (err) {
+                        msg.channel.send("Error linking your Discord and Steam accounts. This is most likely a problem with the database.");
+                        return resolve();
+                    }
+
+                    msg.channel.send(`Successfully linked \`${steamIDTestResult}\` to \`${msg.author.tag}\`.`);
+                });
+            } else {
+                user.steamID = steamIDTestResult;
+
+                user.save(err => {
+                    if (err) {
+                        msg.channel.send("Error linking your Discord and Steam accounts. This is most likely a problem with the database.");
+                        return resolve();
+                    }
+
+                    msg.channel.send(`Successfully overrode old Steam ID with \`${steamIDTestResult}\` for \`${msg.author.tag}\`.`);
+
+                    resolve();
+                });
+            }
+        });
     });
 }
