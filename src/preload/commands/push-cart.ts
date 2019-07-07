fdea04eda7a69d495a2a2a7261f59a7d3b366b12
Juan de Urtubey <jdeurt@gmail.com>
2019-07-07 15:02:00 -0500

    Fix.

--- src/preload/commands/push-cart.ts
+++ src/preload/commands/push-cart.ts
@@ -32,8 +32,6 @@ export async function run(bot: Bot, msg: Message) {
         server.save()
     ]);
 
-    await server.refresh();
-
     pushMessage.delete();
-    msg.channel.send(`<:payload:597506053021630464> Pushed the cart forward **${feetPushed}** feet (${Math.round(5280 * server.server.fun!.payloadMilesPushed)} total).`);
+    msg.channel.send(`<:payload:597506053021630464> Pushed the cart forward **${feetPushed}** feet (${server.server.fun!.payloadMilesPushed} miles total).`);
 }
