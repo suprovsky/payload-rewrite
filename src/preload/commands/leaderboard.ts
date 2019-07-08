9d5567c4f306458cd8c989fcd7a68abbf2acbd76
Juan de Urtubey <jdeurt@gmail.com>
2019-07-08 17:35:42 -0500

    Got rid of embed.

--- src/preload/commands/leaderboard.ts
+++ src/preload/commands/leaderboard.ts
@@ -16,7 +16,7 @@ export async function run(bot: Bot, msg: Message) {
     let top10 = bot.leaderboard.users.slice(0, 10);
 
     let isTop10 = false;
-    let leaderboardString = "```md\n";
+    let leaderboardString = "Pushcart Leaderboard\n```md\n";
 
     for (let i = 0; i < top10.length; i++) {
         let tag = (bot.users.get(top10[i].id) || await bot.fetchUser(top10[i].id)).tag;
@@ -31,11 +31,7 @@ export async function run(bot: Bot, msg: Message) {
 
     if (!isTop10) leaderboardString += `...\n> ${bot.leaderboard.users.findIndex(user => user.id == msg.author.id) + 1}: ${msg.author.tag} (${(bot.leaderboard.users.find(user => user.id == msg.author.id) || { pushed: 0 }).pushed})\n\`\`\``;
 
-    msg.channel.send(new RichEmbed({
-        title: "Pushcart Leaderboard",
-        description: leaderboardString,
-        footer: {
-            text: `Last updated: ${bot.leaderboard.updated.toLocaleString()}`
-        }
-    }));
+    leaderboardString += `\nLast updated: ${bot.leaderboard.updated.toLocaleString()}`;
+
+    msg.channel.send(leaderboardString);
 }
