ad3fd6068fbd91ed142fedb1ee5547c00b850b29
Juan de Urtubey <jdeurt@gmail.com>
2019-07-08 10:42:05 -0500

    Better restrict syntax.

--- src/preload/commands/restrict.ts
+++ src/preload/commands/restrict.ts
@@ -5,7 +5,7 @@ import { getArgs, sliceCmd } from "../../utils/command-parsing";
 
 export const name = "restrict";
 export const description = "Restricts a command from being used in a channel.";
-export const usage = `${config.PREFIX}${name} <command 1> [command 2]...`;
+export const usage = `${config.PREFIX}${name} <command 1> [command 2]... <command 1> [command 2]... [channel 1] [channel 2]...`;
 export const permissions = ["SEND_MESSAGES"];
 export const canBeExecutedBy = ["SEND_MESSAGES", "ADMINISTRATOR"];
 export const zones = ["text"];
@@ -13,16 +13,33 @@ export const zones = ["text"];
 export async function run(bot: Bot, msg: Message) {
     let args = getArgs(sliceCmd(msg, name));
 
-    if (args.includes(name)) return msg.channel.send("Restricting this command from being used here is probably not what you want to do...");
+    let commands: Array<string> = [];
+    let channels: Array<string> = [];
+
+    for (let i = 0; i < args.length; i++) {
+        if (args[i].match(/^<#\d+>$/)) channels.push(args[i].slice(2, -1));
+        else commands.push(args[i]);
+    }
+
+    if (channels.length == 0) {
+        channels = [ msg.channel.id ];
+    }
+
+    if (commands.includes(name)) return msg.channel.send("Restricting this command from being used here is probably not what you want to do...");
 
     let serverManager = bot.serverManager;
     let server = await serverManager.ensureServer(msg.guild.id);
 
-    server.addCommandRestrictions([
-        { channelID: msg.channel.id, commands: args }
-    ]);
+    server.addCommandRestrictions(
+        channels.map(channelID => {
+            return {
+                channelID,
+                commands
+            };
+        })
+    );
 
     await server.save();
 
-    msg.channel.send(`Restricted in ${msg.channel.toString()}: \`\`\`${args.join("\n")}\`\`\``);
+    msg.channel.send(`Restricted in ${channels.map(channelID => `<#${channelID}>`).join(", ")}: \`\`\`${commands.join("\n")}\`\`\``);
 }
--- src/preload/commands/unrestrict.ts
+++ src/preload/commands/unrestrict.ts
@@ -5,7 +5,7 @@ import { getArgs, sliceCmd } from "../../utils/command-parsing";
 
 export const name = "unrestrict";
 export const description = "Unrestricts a command from being used in a channel.";
-export const usage = `${config.PREFIX}${name} <command 1> [command 2]...`;
+export const usage = `${config.PREFIX}${name} <command 1> [command 2]... [channel 1] [channel 2]...`;
 export const permissions = ["SEND_MESSAGES"];
 export const canBeExecutedBy = ["SEND_MESSAGES", "ADMINISTRATOR"];
 export const zones = ["text"];
@@ -13,14 +13,31 @@ export const zones = ["text"];
 export async function run(bot: Bot, msg: Message) {
     let args = getArgs(sliceCmd(msg, name));
 
+    let commands: Array<string> = [];
+    let channels: Array<string> = [];
+
+    for (let i = 0; i < args.length; i++) {
+        if (args[i].match(/^<#\d+>$/)) channels.push(args[i].slice(2, -1));
+        else commands.push(args[i]);
+    }
+
+    if (channels.length == 0) {
+        channels = [ msg.channel.id ];
+    }
+
     let serverManager = bot.serverManager;
     let server = await serverManager.ensureServer(msg.guild.id);
 
-    server.removeCommandRestrictions([
-        { channelID: msg.channel.id, commands: args }
-    ]);
+    server.removeCommandRestrictions(
+        channels.map(channelID => {
+            return {
+                channelID,
+                commands
+            };
+        })
+    );
 
     await server.save();
 
-    msg.channel.send(`Unrestricted in ${msg.channel.toString()}: \`\`\`${args.join("\n")}\`\`\``);
+    msg.channel.send(`Unrestricted in ${channels.map(channelID => `<#${channelID}>`).join(", ")}: \`\`\`${commands.join("\n")}\`\`\``);
 }
