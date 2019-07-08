3625042822d6d199e9f312010b48f6b1e61f68e0
Juan de Urtubey <jdeurt@gmail.com>
2019-07-08 10:50:01 -0500

    Changed to #{all}.

--- src/preload/commands/restrict.ts
+++ src/preload/commands/restrict.ts
@@ -4,7 +4,7 @@ import config from "../../../secure-config";
 import { getArgs, sliceCmd } from "../../utils/command-parsing";
 
 export const name = "restrict";
-export const description = "Restricts a command from being used in a channel. Using `#[all]` as a channel argument restricts the commands in all text channels.";
+export const description = "Restricts a command from being used in a channel. Using `#{all}` as a channel argument restricts the commands in all text channels.";
 export const usage = `${config.PREFIX}${name} <command 1> [command 2]... <command 1> [command 2]... [channel 1] [channel 2]...`;
 export const permissions = ["SEND_MESSAGES"];
 export const canBeExecutedBy = ["SEND_MESSAGES", "ADMINISTRATOR"];
@@ -18,7 +18,7 @@ export async function run(bot: Bot, msg: Message) {
 
     for (let i = 0; i < args.length; i++) {
         if (args[i].match(/^<#\d+>$/)) channels.push(args[i].slice(2, -1));
-        else if (args[i].toLowerCase() == "#[all]") channels.push(...msg.guild.channels.filter(channel => channel.type == "text").map(channel => channel.id));
+        else if (args[i].toLowerCase() == "#{all}") channels.push(...msg.guild.channels.filter(channel => channel.type == "text").map(channel => channel.id));
         else commands.push(args[i]);
     }
 
