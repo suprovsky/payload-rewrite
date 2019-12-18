19e21d35c10c428296280df7c27bca535c768135
Radosław Serba <radoslaw@serba.ovh>
2019-12-19 00:35:55 +0100

    changed a config needed for the tests

--- test/bot.ts
+++ test/bot.ts
@@ -1,7 +1,7 @@
 import { describe, it } from "mocha";
 import { expect } from "chai";
 import bot from "../src/bot";
-import cfg from "../secure-config";
+import cfg from "../example-config";
 
 describe("Payload", () => {
 });
