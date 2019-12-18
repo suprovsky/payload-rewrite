ba6019ab86903b5e2099bd6af930bc63c318305c
Radosław Serba <radoslaw@serba.ovh>
2019-12-19 00:38:54 +0100

    Update bot.ts

--- test/bot.ts
+++ test/bot.ts
@@ -1,7 +1,7 @@
 import { describe, it } from "mocha";
 import { expect } from "chai";
 import bot from "../src/bot";
-import cfg from "../example-config";
+import cfg from "../secure-config";
 
 describe("Payload", () => {
 });
